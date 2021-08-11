f885f938525755da5d9fe5556c0bb12d636804bc
unknown <ssgsj.carranza@gmail.com>
2021-08-10 20:49:25 -0500

    installed hero icons and pulled in search icon and added styling, woekiong on farther styling of search bar element

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image';
+import {SearchIcon} from '@heroicons/react/solid';
 
 function Header() {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* left section */}
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src='https://links.papareact.com/qd3' 
@@ -12,8 +13,9 @@ function Header() {
                         objectPosition='left'/>        
             </div>
             {/* Middle section */}
-            <div>
-
+            <div className='flex items-center md:border-2 rounded-full py-2'>
+                <input className="pl-5 bg-transparent outline-none" type="text" placeholder="Start your Search" />
+                <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer"/>
             </div>
             {/* right section */}
             <div>
