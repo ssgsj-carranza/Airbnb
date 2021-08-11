cbb56556cbd44252451a572041a60d5d66614323
unknown <ssgsj.carranza@gmail.com>
2021-08-10 20:55:37 -0500

    finished styling of search bar, working on right section of header component next

--- components/Header.js
+++ components/Header.js
@@ -13,9 +13,9 @@ function Header() {
                         objectPosition='left'/>        
             </div>
             {/* Middle section */}
-            <div className='flex items-center md:border-2 rounded-full py-2'>
-                <input className="pl-5 bg-transparent outline-none" type="text" placeholder="Start your Search" />
-                <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer"/>
+            <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your Search" />
+                <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
             {/* right section */}
             <div>
