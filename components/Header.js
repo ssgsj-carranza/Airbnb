cbdf7e9075da5285b415e9c984510c6e47bafe4b
unknown <ssgsj.carranza@gmail.com>
2021-08-10 20:35:35 -0500

    begain styling out image logo being pulled into the header, and a bit of the header as well

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,13 @@ import Image from 'next/image';
 
 function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
             {/* left section */}
-            <div>
-                <Image src='https://links.papareact.com/qd3' layout='fill'/>        
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image src='https://links.papareact.com/qd3' 
+                        layout='fill' 
+                        objectFit='contain'
+                        objectPosition='left'/>        
             </div>
             {/* Middle section */}
             <div>
