7c693c22909a028698ad9784d3d69bd249c977ae
unknown <ssgsj.carranza@gmail.com>
2021-08-10 20:27:36 -0500

    created next.config file to allow images being pulled from cedrtain domains

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Image from 'next/image';
 
 function Header() {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <h1>Header</h1>
+            {/* left section */}
+            <div>
+                <Image src='https://links.papareact.com/qd3' layout='fill'/>        
+            </div>
+            {/* Middle section */}
+            <div>
+
+            </div>
+            {/* right section */}
+            <div>
+
+            </div>
+        </header>
     )
 }
 
