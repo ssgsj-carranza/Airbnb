557800be9b6394a332a7d901dd43ef64468dccc3
unknown <ssgsj.carranza@gmail.com>
2021-08-11 20:20:04 -0500

    added section for explore nearby locations and styled heading, pulling location from endpoint using server side rendering

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,12 @@ export default function Home() {
       </Head>
         <Header />
         < Banner />
+        <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+          <section className="pt-6">
+            <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+            {/* pull data from server - API endpoint */}
+          </section>
+        </main>
     </div>
   )
 }
