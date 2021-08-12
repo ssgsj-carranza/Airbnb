794a34fe6510d25247693fc575378ecedd7007ed
unknown <ssgsj.carranza@gmail.com>
2021-08-11 20:01:52 -0500

    pulled in banner image and added styling to it, made it screen responsive using jit, continuing banner page work

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from 'next/image';
 
 function Banner() {
     return (
-        <div>
-            
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+            <Image src='https://links.papareact.com/0fm'
+                    layout='fill'
+                    objectFit='cover'/>
         </div>
     )
 }
