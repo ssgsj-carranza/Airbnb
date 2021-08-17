3b5a428d2145ea941626dff978a236ceecf53b80
unknown <ssgsj.carranza@gmail.com>
2021-08-16 21:41:15 -0500

    began working on search functionality, set up a useState to track the users input and an onChange function to allow typing

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Image from 'next/image';
 import {SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon} from '@heroicons/react/solid';
+import {useState} from 'react';
 
 function Header() {
+    const [searchInput, setSearchInput] = useState('');
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* left section */}
@@ -12,9 +15,9 @@ function Header() {
                         objectFit='contain'
                         objectPosition='left'/>        
             </div>
-            {/* Middle section */}
+            {/* Middle section--Search */}
             <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your Search" />
+                <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your Search" />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
             {/* right section */}
