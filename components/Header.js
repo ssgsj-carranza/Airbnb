2e5f51b8ff20df85f5234bf6e1f47f107848c2f8
unknown <ssgsj.carranza@gmail.com>
2021-08-16 22:11:06 -0500

    styled calender div to display at the center of the screen

--- components/Header.js
+++ components/Header.js
@@ -46,7 +46,7 @@ function Header() {
                 </div>   
             </div>
             {searchInput && (
-                <div>
+                <div className='flex flex-col col-span-3 mx-auto'>
                     <DateRangePicker ranges={[selectionRange]}
                                      minDate={new Date()}
                                      rangeColors={['#FD5B61']}
