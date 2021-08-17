69452bd49fdc1c153658ef1f1c92cc928fc5699a
unknown <ssgsj.carranza@gmail.com>
2021-08-16 21:08:00 -0500

    pulled in large card image from link, workin on styling relative to the page, pulling title, and description next, as well as styling

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Image from 'next/image'; 
 
 function LargeCard({img, title, description, buttonText}) {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className="relative h-96 min-w-[300px]">
+                <Image src={img} layout='fill' objectFit='cover'/>
+            </div>
+            <div>
+
+            </div>
+        </section>
     )
 }
 
