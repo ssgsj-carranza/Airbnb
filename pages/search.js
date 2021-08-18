4844c82b040248a6d4b24318bcc0e9132946d930
unknown <ssgsj.carranza@gmail.com>
2021-08-17 21:31:51 -0500

    pulled header and footer componenet, added some titles and p tags to display messages, began styling as well

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 function Search() {
     return (
         <div>
-            <h1>Search Page</h1>            
+            <Header />
+            <main className="flex">
+                <section>
+                    <p className="text-xs">300+ Stays for ...</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Places</h1>
+                    <div className="hidden lg:inline-flex">
+                        <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg">Cancellation Flexibility</p>
+                    </div>
+                </section>
+            </main>
+            <Footer />            
         </div>
     )
 }
