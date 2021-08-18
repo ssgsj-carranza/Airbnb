dc62d0da6d3d6c132b1e77884aa4a0c579ef92b0
unknown <ssgsj.carranza@gmail.com>
2021-08-17 21:12:38 -0500

    added pieces of state to track number of selected guests and added onChange event to input field

--- components/Header.js
+++ components/Header.js
@@ -10,6 +10,7 @@ function Header() {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
     
     const selectionRange = {
         startDate: startDate,
@@ -55,7 +56,7 @@ function Header() {
                     <div className='flex items-center border-b mb-4'>
                         <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
                         <UsersIcon className='h-5'/>
-                        <input type="number" className='w-12 pl-2 text-lg outline-none text-red-400'/>
+                        <input value={noOfGuests} onChange={(e) => setNoOfGuests(e.target.value)} min={1} type="number" className='w-12 pl-2 text-lg outline-none text-red-400'/>
                     </div>
                 </div>
             )}
