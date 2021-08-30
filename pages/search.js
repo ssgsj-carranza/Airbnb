d9532a318afc9557a6f48f317bd92b463685484a
unknown <ssgsj.carranza@gmail.com>
2021-08-30 18:40:47 -0500

    added different nav buttons using the created button utility class, and added styling using mobile first response so that the buttons are hidden and only appear on a large screen

--- pages/search.js
+++ pages/search.js
@@ -9,9 +9,13 @@ function Search() {
             <main className="flex">
                 <section>
                     <p className="text-xs">300+ Stays for ...</p>
-                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Places</h1>
-                    <div className="hidden lg:inline-flex">
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in Places</h1>
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>
                     </div>
                 </section>
             </main>
