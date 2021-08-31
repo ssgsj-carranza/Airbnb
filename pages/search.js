529b3f7d447847305bc2a71892a87fd512b40558
unknown <ssgsj.carranza@gmail.com>
2021-08-30 19:11:31 -0500

    created 3 constants, one is the formatted start date to convert the string from the start and end date into corresponding format dd/mmmm/yy using date-fns package and following documentation, created a const to show the range from the start to end date, and called that const into the p tag displaying search information

--- pages/search.js
+++ pages/search.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import {useRouter} from 'next/dist/client/router';
+import {format} from 'date-fns';
 
 function Search() {
     const router = useRouter();
@@ -9,12 +10,16 @@ function Search() {
     //ES6 DESTRUCTURING
     const {location, startDate, endDate, noOfGuests} = router.query;
 
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
+
     return (
         <div>
             <Header />
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
-                    <p className="text-xs">300+ Stays for ...</p>
+                    <p className="text-xs">300+ Stays - {range} - for {noOfGuests} guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in {location}</h1>
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
