f18124dfafcc12c16812ece6d799bbfbb9057bef
unknown <ssgsj.carranza@gmail.com>
2021-08-30 19:38:41 -0500

    created info card component to display the search results img, location, price, etc.. created a map function and destructured it, and mapped through all the elements in the data received from the api, working on displaying information in the info card component

---
+++ components/InfoCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InfoCard({img, location, title, description, star, price, total}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InfoCard
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import {useRouter} from 'next/dist/client/router';
 import {format} from 'date-fns';
+import InfoCard from '../components/InfoCard';
 
 function Search({searchResults}) {
     const router = useRouter();
@@ -27,6 +28,20 @@ function Search({searchResults}) {
                         <p className="button">Price</p>
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More filters</p>
+                    </div>
+                    <div className="flex flex-col">
+                        {searchResults.map(({img, location, title, description, star, price, total}) => (
+                            <InfoCard
+                                key={img} 
+                                img={img}
+                                location={location}
+                                title={title}
+                                description={description}
+                                star={star}
+                                price={price}
+                                total={total}
+                            />        
+                        ))}
                     </div>
                 </section>
             </main>
