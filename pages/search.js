6bda27106a4836ac1d73ca9f5ad310070535f791
unknown <ssgsj.carranza@gmail.com>
2021-08-30 19:04:56 -0500

    used es6 destruturing to pull the router query information, succesfully pulled in location name based on user input

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import {useRouter} from 'next/dist/client/router';
 
 function Search() {
+    const router = useRouter();
+    
+    //ES6 DESTRUCTURING
+    const {location, startDate, endDate, noOfGuests} = router.query;
+
     return (
         <div>
             <Header />
             <main className="flex">
                 <section className="flex-grow pt-14 px-6">
                     <p className="text-xs">300+ Stays for ...</p>
-                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in Places</h1>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stay in {location}</h1>
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
                         <p className="button">Type of Place</p>
