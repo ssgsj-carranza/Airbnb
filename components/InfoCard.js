a807936ed2c3280b107b0421bfa458f2c84f8da5
unknown <ssgsj.carranza@gmail.com>
2021-08-30 19:46:48 -0500

    pulled in images and began adding a little styling to them

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from 'next/image';
 
 function InfoCard({img, location, title, description, star, price, total}) {
     return (
         <div>
-            
+            <div className="relative h-24 w-40">
+                <Image src={img} layout='fill' objectFit='cover'/>
+            </div>
         </div>
     )
 }
