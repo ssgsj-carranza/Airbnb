5fdc8847b49b67f1fc2e3a852d846ac7d20797fe
unknown <ssgsj.carranza@gmail.com>
2021-08-31 20:43:40 -0500

    added center point of all places for rent available in the back end, to be displayed on map, so the map now shows relative area, working on adding markers next for each available place

--- components/Map.js
+++ components/Map.js
@@ -3,18 +3,23 @@ import ReactMapGL from 'react-map-gl';
 import getCenter from 'geolib/es/getCenter';
 
 function Map({searchResults}) {
-    const [viewport, setViewport] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 11,
-    });
+    
 //TRANSFORM SEARCHRESULTS OBJECT INTO THE OBJECT REQUIRED
     const coordinates = searchResults.map((result) => ({
         longitude: result.long,
         latitude: result.lat,
     }));
+
+//THE LATITUDE AND LONGITUDE OF THE CENTER OF LOCATIONS COORDINATES
+    const center = getCenter(coordinates);
+
+    const [viewport, setViewport] = useState({
+        width: '100%',
+        height: '100%',
+        latitude: center.latitude,
+        longitude: center.longitude,
+        zoom: 11,
+    });
 
     return (
         <ReactMapGL
