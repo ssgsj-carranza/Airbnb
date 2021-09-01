33ea46836a8efeeceef5d8ae303a4455feaf3f33
unknown <ssgsj.carranza@gmail.com>
2021-08-31 19:50:43 -0500

    added mapbox key variable to config file to hold public key, used process to pull public key in map component

--- components/Map.js
+++ components/Map.js
@@ -3,9 +3,12 @@ import ReactMapGL from 'react-map-gl';
 
 function Map() {
     return (
-        <div>
-            
-        </div>
+        <ReactMapGL
+            mapStyle='mapbox://styles/ssgsj-carranza/ckt0ritab02o917k71u8sjv2i'
+            mapboxApiAccessToken={process.env.mapbox_key}
+        >
+
+        </ReactMapGL>
     )
 }
 
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com'],
     },
+    env: {
+        mapbox_key:'pk.eyJ1Ijoic3Nnc2otY2FycmFuemEiLCJhIjoiY2t0MHJkYXl6MDZndjJ1bnh1bzk5dDF2ciJ9.7-CU3MzM0m3-1epPRPNIAg'
+    }
 };
